eb23f45010dc97a23f974d96e2b8d3680e7344d8
Zach <zachery.ca@gmail.com>
2021-04-28 11:41:01 -0500

    started on the form

--- NanominerGUI/Form1.Designer.cs
+++ NanominerGUI/Form1.Designer.cs
@@ -29,13 +29,103 @@ namespace NanominerGUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBoxETCAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonLoad = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxETCAddress
+            // 
+            this.textBoxETCAddress.Location = new System.Drawing.Point(12, 27);
+            this.textBoxETCAddress.Name = "textBoxETCAddress";
+            this.textBoxETCAddress.Size = new System.Drawing.Size(305, 23);
+            this.textBoxETCAddress.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ethereum Classic (ETC) Address:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Email:";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(12, 84);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(305, 23);
+            this.textBoxEmail.TabIndex = 3;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(12, 126);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(101, 47);
+            this.buttonSave.TabIndex = 4;
+            this.buttonSave.Text = "Save to File";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonLoad
+            // 
+            this.buttonLoad.Location = new System.Drawing.Point(12, 179);
+            this.buttonLoad.Name = "buttonLoad";
+            this.buttonLoad.Size = new System.Drawing.Size(101, 47);
+            this.buttonLoad.TabIndex = 5;
+            this.buttonLoad.Text = "Load from File";
+            this.buttonLoad.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(119, 126);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(198, 100);
+            this.buttonStart.TabIndex = 6;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(329, 238);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonLoad);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxETCAddress);
+            this.Name = "Form1";
+            this.Text = "Nanominer GUI by Zachonite";
+            this.Load += new System.EventHandler(this.form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBoxETCAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonLoad;
+        private System.Windows.Forms.Button buttonStart;
     }
 }
 
--- NanominerGUI/Form1.cs
+++ NanominerGUI/Form1.cs
@@ -7,15 +7,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Net;
 
 namespace NanominerGUI
 {
     public partial class Form1 : Form
     {
+        public string thisLoc = Application.StartupPath;
+        public string strETCaddress;
+
         public Form1()
         {
             InitializeComponent();
         }
+        private void form1_Load(object sender, EventArgs e)
+        {
+            //testing
+            textBoxETCAddress.Text = "0x98bdf1563461dca87f6e5fe9e5ad75dc4147b313";
+        }
 
+        private void buttonValidateETC_Click(object sender, EventArgs e)
+        {
+        }
     }
 }
